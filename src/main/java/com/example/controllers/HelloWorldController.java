4ccd919f9ae9b49154002485c931377bab7d704a
Hasan <hasan.bin.alam.tanim@gmail.com>
2020-10-31 00:49:56 +0600

    Updated Message with Student information

--- src/main/java/com/example/controllers/HelloWorldController.java
+++ src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,14 +1,19 @@
 package com.example.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.dto.Student;
+
 @RestController
 @RequestMapping("/api/v1")
 public class HelloWorldController {
+	@Autowired
+	Student std;
 	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World";
+	public Student hello() {
+		return std;
 	}
 }
---
+++ src/main/java/com/example/dto/Student.java
@@ -0,0 +1,49 @@
+package com.example.dto;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	@Value("0107039")
+	private String id;
+	@Value("Hasan Bin Alam")
+	private String name;
+	@Value("UI Govt. High SChool")
+	private String school;
+	@Value("SSC")
+	private String standard;
+	@Value("10-03-83")
+	private String dob;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String getStandard() {
+		return standard;
+	}
+	public void setStandard(String standard) {
+		this.standard = standard;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	
+}
